ac5345110fc0e93823a73957e0f4e93115bcac4e
Gowtham <114149222+gowtham2303@users.noreply.github.com>
2024-07-20 15:33:55 +0530

    2day

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,32 @@
+"use client"
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/Sidebar";
+import Image from 'next/image'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIN = { firstname :'Gowtham' , lastname: "Tharigopula "};
   return (
-   <main>
-      SiDEBAR
-     {children}
+   <main className="flex h-screen w-full font-inter ">  
+     <SideBar user= {loggedIN} />
+     <div className="size-full flex-col">
+      <div className="root-layout ">
+        <Image src="/icons/logo.svg"
+        width={30}
+        height={30}
+        alt="menu"
+        />
+        <div>
+          <MobileNav user={loggedIN} />
+
+        </div>
+      </div>
+      {children}
+     </div>
+
 
    </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,34 @@
 // pages/index.js (or index.tsx)
 
+import ToolBalanceBox from '@/components/ToolBalanceBox';
+import HeaderBox from '@/components/HeaderBox';
 import React from 'react';
 
 const Home = () => {
+    const loggedIn ={firstName : 'Gowtham '};
+
   return (
-    <div>
-       Home
-    </div>
+    <section className='home'>
+       <div className='home-content'>    
+       <header className="home-header">
+         <HeaderBox 
+         type='greeting'
+         title="Welcome"
+         user={loggedIn?.firstName || 'Guest'}
+         subtext ='Access and Manage Your account and Transactions efficeiently .'
+         />
+         <ToolBalanceBox
+         accounts ={[]}
+         totalBanks={1}
+         totalCurrentBalance = {1250.35}
+         />
+          
+
+       </header>
+       </div>
+    </section>
   );
 };
+
 
 export default Home;
